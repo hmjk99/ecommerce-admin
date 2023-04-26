d1dacc1ce8e4f1d67b4a63afc0be61e4fdf8019c
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-04-25 22:48:40 -0400

    finished imageField and basic Crud function

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,16 +8,17 @@ export default function ProductForm({
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
-    images,
+    images:existingImages,
 }) {
     const [title, setTitle] = useState(existingTitle || '')
     const [description, setDescription] = useState(existingDescription || '')
     const [price, setPrice] = useState(existingPrice || '')
+    const [images, setImages] = useState(existingImages || [])
     const [goToProducts, setGoToProducts] = useState(false)
     const router = useRouter()
 
     async function saveProduct(e) {
-        const data = {title,description,price}
+        const data = {title,description,price,images}
         e.preventDefault()
         if (_id){
             //update
@@ -33,8 +34,13 @@ export default function ProductForm({
         const files = e.target?.files
         if (files?.length > 0){
             const data = new FormData()
-            files.forEach(file => data.append('file', file))
+            for (const file of files) {
+                data.append('file', file)
+            }
             const res = await axios.post('/api/upload', data)
+            setImages(oldImages =>{
+                return [...oldImages, ...res.data.links]
+            })
         }
     }
 
@@ -47,7 +53,12 @@ export default function ProductForm({
             <label>Product Name</label>
             <input type='text' placeholder='product name' value={title} onChange={e => setTitle(e.target.value)}/>
             <label>Images</label>
-            <div className='mb-2'>
+            <div className='mb-2 flex flex-wrap gap-2'>
+                {!!images?.length && images.map(link =>(
+                    <div className='h-24' key={link}>
+                        <img src={link} alt="" className='rounded-lg'/>
+                    </div>
+                ))}
                 <label className='cursor-pointer w-24 h-24 flex flex-col items-center justify-center text-gray-500 rounded-lg bg-gray-200'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
@@ -55,9 +66,6 @@ export default function ProductForm({
                     <div>Upload</div>
                     <input type="file" className='hidden'onChange={uploadImages}/>
                 </label>
-                {!images?.length && (
-                    <div>No photos in this product</div>
-                )}
             </div>
             <label>Description</label>
             <textarea placeholder='description' value={description} onChange={e => setDescription(e.target.value)}></textarea>
